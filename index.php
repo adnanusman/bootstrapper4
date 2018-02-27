336ef216e58102d2800be08c45e93fd701cc86d5
Adnan Usman <adnantu@hotmail.com>
2018-02-26 17:34:10 -0800

    added html structure to index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>BootStrapper 4</title>
+
+</head>
+    
+<body <?php body_class(); ?>>
+      
+    <header>
+    
+        <section class="top-bar">
+        
+            <div class="social-media-icons">
+            </div>
+            
+            <div class="search">
+            </div>
+        
+            <section class="menu-area">
+                <section class="logo"></section>
+                
+                <nav class="main-menu"></nav>
+            </section>
+            
+        </section>
+        
+    </header>
+    
+    <div class="content-area">
+        <main>
+        
+            <section class="slider"></section>
+            <section class="services"></section>
+            <section class="middle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>
+            
+            <section class="map"></section>
+        
+        </main>
+    </div>
+
+    <footer></footer>
+    
+</body>    
+</html>
