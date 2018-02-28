daad656fa06dfd6538da319fc6c63656bc36723f
Adnan Usman <adnantu@hotmail.com>
2018-02-28 13:05:26 -0800

    Added Custom Template Skeletons

---
+++ fullwidth-template.php
@@ -0,0 +1,53 @@
+<?php 
+/*
+Template Name: Full Width Template
+*/
+?>
+
+<?php get_header(); ?>
+    <div class="content-area">
+        <main>
+            <section="middle-area">
+                <div class="container">
+
+                
+                <div class="general-template">
+                    
+                    <?php
+                    // If there are any posts
+                    if ( have_posts() ):
+                        // Then while Posts exist, Display the posts
+                        while( have_posts() ): the_post();
+                    ?>
+                    
+                    <article>
+                    
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php the_content(); ?></p>
+
+                    </article>
+                    
+                    <?php
+                    
+                    // endwhile --- the first if condition is still active
+                    endwhile;
+                    // now the second condition begins, in case there are no posts
+                    else:
+                    ?>
+                                        
+                    <p>There is nothing to display.</p>
+                    
+                    <?php
+                    // if statement ends
+                    endif;
+                    ?>
+                </div>
+            
+                    </div>
+                </div>                
+            </section>
+
+        
+        </main>
+    </div>
+<?php get_footer(); ?>
---
+++ page-home.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+    <div class="content-area">
+        <main>
+        
+            <section class="slider">
+                
+                <div class="container">
+                    <div class="row text-center col-xs-12">
+                
+                    Slider Area        
+                    
+                    </div>
+                </div>
+                
+            </section>
+            <section class="services">
+                
+                <div class="container">
+                    <div class="row">
+                                
+                    Services
+                
+                    </div>
+                </div>    
+            </section>
+            
+            <section class="middle-area">
+                <div class="container">
+                    <div class="row">
+                
+                <aside class="sidebar col-md-3">
+                    Sidebar
+                </aside>
+                
+                <div class="news col-md-9">
+
+                    This is the homepage news section.
+
+                </div>
+            
+                    </div>
+                </div>                
+            </section>
+            
+            <section class="map">
+                <div class="container">
+                    <div class="row">
+                    
+                        Map Area
+                
+                    </div>
+                </div>
+            </section>
+        
+        </main>
+    </div>
+<?php get_footer(); ?>
