fa3039b05a5f6d7dbb00cf617ae92dc693ec517a
Adnan Usman <adnantu@hotmail.com>
2018-02-28 17:22:00 -0800

    added meta-info class

--- components/blog-content.php
+++ components/blog-content.php
@@ -1,13 +1,17 @@
 <article <?php post_class() ?>>
 
     <h2><?php the_title(); ?></h2>
-    <?php // check if the post has a Post Thumbnail assigned to it.
-    if ( has_post_thumbnail() ) {
-        the_post_thumbnail( 'blog-thumbnail' );
-    } ?>
-    <p>Posted on <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-    <p>Categories: <?php the_category(' | '); ?></p>
-    <p><?php the_tags( 'Tags: ' , ', ' ); ?></p>
-    <p><?php the_content(); ?></p>
+        <?php // check if the post has a Post Thumbnail assigned to it.
+        if ( has_post_thumbnail() ) {
+            the_post_thumbnail( 'blog-thumbnail' );
+        } ?>
+
+    <div class="meta-info">
+        <p>Posted on <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+        <p>Categories: <?php the_category(' | '); ?></p>
+        <p><?php the_tags( 'Tags: ' , ', ' ); ?></p>
+    </div>
+    
+        <p><?php the_content(); ?></p>
 
 </article>
